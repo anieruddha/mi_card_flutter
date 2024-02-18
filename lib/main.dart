ecd93efcf89cd112e4dcfd346feea2778b265c6c
anieruddha <anieruddha@gmail.com>
2024-02-18 16:38:06 -0600

    using card & list tile

--- lib/main.dart
+++ lib/main.dart
@@ -42,10 +42,58 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'sourcesans',
                     fontSize: 18,
                     color: Colors.teal.shade200,
-                    fontWeight: FontWeight.bold,
-                    decoration: TextDecoration.underline
+                    fontWeight: FontWeight.bold
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal.shade200,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15
+                  ),
+                  color: Colors.white,
+                  child: ListTile(
+                    dense: true,
+                    leading:
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal.shade900,
+                      ),
+                    title: Text(
+                        '+1 123 456 7890',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15
+                  ),
+                  color: Colors.white,
+                  child: ListTile(
+                    dense: true,
+                    leading:
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'tjdetweiler@grade5.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
